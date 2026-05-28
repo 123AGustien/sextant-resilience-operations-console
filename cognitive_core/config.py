29686a37d3e7d59e2fcdf31e4b85ba89c28e2a5a
Mr.Don Herman Oswald Weerasekera <donweerasekera@gmail.com>
2026-05-28 19:35:40 +0700

    Create config.py

---
+++ cognitive_core/config.py
@@ -0,0 +1,13 @@
+# cognitive_core/config.py
+
+AGENT_ROLES = [
+    "observer",
+    "diagnoser",
+    "validator",
+    "recovery_planner"
+]
+
+MEMORY_RETENTION_LIMIT = 1000
+
+CONFIDENCE_THRESHOLD = 0.75
+ENABLE_SELF_CORRECTION = True
